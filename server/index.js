11e7a09821d7007d9e4b3259e1e384f2041ac9ea
PanaitCosmin <cosmindaniel.panait@gmail.com>
2025-02-15 20:45:29 +0200

    Credentials header

--- server/index.js
+++ server/index.js
@@ -23,6 +23,11 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
+// Ensure response headers allow credentials
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 // Image Upload Route
 app.post('/api/upload', upload.single('image'), async (req, res) => {
