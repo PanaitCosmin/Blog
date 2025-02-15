a71779e6c753b2927948a738efb00e738d2d1b0a
PanaitCosmin <cosmindaniel.panait@gmail.com>
2025-02-15 18:48:44 +0200

    Add connection pool

--- server/db.js
+++ server/db.js
@@ -1,8 +1,10 @@
-import mysql from 'mysql2'
+import mysql from 'mysql2/promise'
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
     host: process.env.HOST_DB,
     user: process.env.USER_DB,
     password: process.env.PASSWORD_DB,
     database: process.env.DB,
+    waitForConnections: 10,
+    queueLimit: 0
 })
