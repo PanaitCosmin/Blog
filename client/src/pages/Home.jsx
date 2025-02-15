6b91ce8295229d67965257865aee4278f8ea457f
PanaitCosmin <cosmindaniel.panait@gmail.com>
2025-02-15 17:35:53 +0200

    Implement skeleton loading

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,16 +5,20 @@ import axios from 'axios'
 const Home = () => {
 
   const [posts, setPosts] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
 
   const cat = useLocation().search
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true)
         const res = await axios.get(`/api/posts${cat}`)
         setPosts(res.data)
       } catch (error) {
         console.log(error)
+      } finally {
+        setIsLoading(false)
       }
     }
 
@@ -31,9 +35,27 @@ const Home = () => {
 
   return (
     <div className="home mt-[15vh] lg:mt-[20vh] min-h-[70vh]">
-        {posts.length === 0 && <div className='min-h-[70vh] flex items-center justify-center text-5xl'><h1>No posts for this category</h1></div>}
+        {posts.length === 0 && !isLoading && (
+        <div className='min-h-[70vh] flex items-center justify-center text-5xl'>
+          <h1>No posts for this category</h1>
+        </div>)}
         <div className="posts space-y-10">
-            {posts.map((post) => ( 
+            {isLoading
+          ? // Skeleton Loading Placeholder
+            Array(3).fill().map((_, index) => (
+              <div key={index} className="post flex flex-col-reverse md:flex-row items-center md:items-start gap-6 md:gap-10 overflow-hidden p-6 animate-pulse">
+                <div className="content flex-1 max-w-lg self-center text-center md:text-left">
+                  <div className="h-6 bg-gray-300 rounded-md w-2/3 mb-4"></div>
+                  <div className="h-4 bg-gray-300 rounded-md w-full mb-2"></div>
+                  <div className="h-4 bg-gray-300 rounded-md w-5/6 mb-2"></div>
+                  <div className="h-4 bg-gray-300 rounded-md w-4/6 mb-4"></div>
+                  <div className="h-10 bg-gray-300 rounded-md w-32"></div>
+                </div>
+                <div className="img w-full md:w-1/2 bg-gray-300 rounded-lg h-60"></div>
+              </div>
+            ))
+            : // Render Posts
+            posts.map((post) => ( 
                 <div key={post.id} className="post flex flex-col-reverse md:flex-row items-center md:items-start gap-6 md:gap-10 overflow-hidden p-6">
                     
                     {/* Text Content */}
