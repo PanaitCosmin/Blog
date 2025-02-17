a58024ed0be17fdb74947aa7957b2063ab4be554
PanaitCosmin <cosmindaniel.panait@gmail.com>
2025-02-17 18:37:30 +0200

    Edit CORS

--- server/index.js
+++ server/index.js
@@ -42,16 +42,16 @@ app.use(cors({
     origin: allowedOrigin,  // Allow only the frontend domain
     credentials: true,      // If using cookies/authentication
     methods: ["GET", "POST", "PUT", "DELETE"], 
-    allowedHeaders: ["content-type", "authorization"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", allowedOrigin);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", allowedOrigin);
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 
 //   app.use(session({
